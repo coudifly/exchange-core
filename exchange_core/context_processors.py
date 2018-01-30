d45d3748753e3834c522ccd9794880b4573030c3
Juliano Gouveia <juliano@neosacode.com>
2018-01-30 10:50:23 -0200

    Context proccessors file identantion enhancements

--- exchange_core/context_processors.py
+++ exchange_core/context_processors.py
@@ -2,4 +2,7 @@ from django.conf import settings
 
 
 def exchange(request):
-	return {'PROJECT_NAME': settings.PROJECT_NAME, 'settings': settings}
+	return {
+		'PROJECT_NAME': settings.PROJECT_NAME,
+		'settings': settings
+	}
