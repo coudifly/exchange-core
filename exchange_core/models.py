65696d2b7bb045fea3d2bc759b6eae2bd521ab8c
Juliano Gouveia <juliano@neosacode.com>
2018-02-06 15:50:02 -0200

    Add tx_id field to statement model

--- exchange_core/models.py
+++ exchange_core/models.py
@@ -158,6 +158,8 @@ class Statement(TimeStampedModel, BaseModel):
     description = models.CharField(max_length=100, verbose_name=_("Description"))
     amount = models.DecimalField(max_digits=20, decimal_places=8, default=Decimal('0.00'))
     type = models.CharField(max_length=30, choices=TYPES, verbose_name=_("Type"))
+    # Campo usado para armazenar o id das transactions e checar se uma transacao ja foi processada ou nao
+    tx_id = models.CharField(max_length=150, null=True, blank=True)
 
     class Meta:
         verbose_name = _("Statement")
