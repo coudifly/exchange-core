c7e965c0fe26dac05102e9f0a0d0228a7a5e22b6
Juliano Gouveia <juliano@neosacode.com>
2018-02-18 11:36:17 -0300

    Remove settings from being a template variable

--- exchange_core/context_processors.py
+++ exchange_core/context_processors.py
@@ -6,5 +6,10 @@ def exchange(request):
 	return {
 		'PROJECT_NAME': settings.PROJECT_NAME,
 		'USER_HAS_DEVICE': lambda: user_has_device(request.user),
-		'settings': settings
+		'BRL_CURRENCY_SYMBOL': settings.BRL_CURRENCY_SYMBOL,
+		'DOMAIN': settings.DOMAIN,
+		'SPONSORSHIP_URL_PREFIX': settings.SPONSORSHIP_URL_PREFIX,
+		'BR_DEPOSIT_MIN': settings.BR_DEPOSIT_MIN,
+		'BR_DEPOSIT_MAX': settings.BR_DEPOSIT_MAX,
+		'BR_DEPOSIT_DAILY_LIMIT': settings.BR_DEPOSIT_DAILY_LIMIT
 	}
