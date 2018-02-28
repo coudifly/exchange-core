c78b3ad2c5c552547149839056576401330091e6
Juliano Gouveia <juliano@neosacode.com>
2018-02-28 15:48:47 -0300

    Exchange django template enhancements

--- exchange_core/context_processors.py
+++ exchange_core/context_processors.py
@@ -8,7 +8,6 @@ def exchange(request):
 		'USER_HAS_DEVICE': lambda: user_has_device(request.user),
 		'BRL_CURRENCY_SYMBOL': settings.BRL_CURRENCY_SYMBOL,
 		'DOMAIN': settings.DOMAIN,
-		'SPONSORSHIP_URL_PREFIX': settings.SPONSORSHIP_URL_PREFIX,
 		'BR_DEPOSIT_MIN': settings.BR_DEPOSIT_MIN,
 		'BR_DEPOSIT_MAX': settings.BR_DEPOSIT_MAX,
 		'BR_DEPOSIT_DAILY_LIMIT': settings.BR_DEPOSIT_DAILY_LIMIT
---
+++ exchange_core/templatetags/exchange.py
@@ -0,0 +1,26 @@
+import json
+import importlib
+
+from django.template import Library
+from django.conf import settings
+from django.urls.resolvers import URLPattern
+
+from exchange_core.utils import get_url_patterns
+
+
+register = Library()
+
+
+@register.filter(is_safe=True)
+def serialize(data):
+    return json.dumps(data)
+
+
+# Armazena o nome das URLs para usar como referencia
+pattern_names = get_url_patterns()
+# Retorna um booleano identificando se a URL existe ou nao
+@register.simple_tag
+def ex_url_exists(pattern_name):
+	if pattern_name in pattern_names:
+		return True
+	return False
