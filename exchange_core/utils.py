7c08ed78670addcd76ce632cfeea9ab32ff62541
Guilherme de S. Vieira <gui_doggao@hotmail.com>
2018-06-08 16:03:49 -0300

    Created close db connection decarator

--- exchange_core/utils.py
+++ exchange_core/utils.py
@@ -2,18 +2,29 @@ import importlib
 
 from django.conf import settings
 from django.urls.resolvers import URLPattern
+from django.db import connection
+from functools import wraps
 
 
 def get_url_patterns():
-	pattern_names = []
+    pattern_names = []
 
-	for app_name in settings.INSTALLED_APPS:
-		if app_name.startswith('exchange_'):
-			urls_module = importlib.import_module(app_name + '.urls')
-			urlpatterns = getattr(urls_module, 'urlpatterns')
-			
-			for url in urlpatterns:
-				if isinstance(url, URLPattern):
-					pattern_names.append(url.pattern.name)
+    for app_name in settings.INSTALLED_APPS:
+        if app_name.startswith('exchange_'):
+            urls_module = importlib.import_module(app_name + '.urls')
+            urlpatterns = getattr(urls_module, 'urlpatterns')
 
-	return pattern_names
+    for url in urlpatterns:
+        if isinstance(url, URLPattern):
+            pattern_names.append(url.pattern.name)
+
+    return pattern_names
+
+
+def close_db_connection(f):
+    @wraps(f)
+    def func_wrapper(*args, **kwargs):
+        f_return = f(*args, **kwargs)
+        connection.close()
+        return f_return
+    return func_wrapper
