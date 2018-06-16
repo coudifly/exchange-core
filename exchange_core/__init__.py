e1b80e139c403a81ba9267a2c711e8cf5e88ca49
Juliano Gouveia <juliano@neosacode.com>
2018-06-16 13:38:21 -0300

    Translate __init__.py from exchange_core to english

--- exchange_core/__init__.py
+++ exchange_core/__init__.py
@@ -10,56 +10,58 @@ from django.conf import settings
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import redirect
+from django.urls import reverse
 from prettyconf.configuration import Configuration
-from prettyconf.exceptions import UnknownConfiguration
 
 from exchange_core.casts import pairs, redis_url
 
-
-# Diz ao pretty conf o path do .env caso não existam variáveis de ambiente para a respectiva config
+# Tells to prettyconf  the path of .env in case of env vars not exists for given configuration
 config = Configuration(starting_path=settings.BASE_DIR)
 
-# Reescreve o enconder Json padrão para suportar Decimal e UUID
+# Overwrite default Json encoder for Decimal and UUID support
 JSONEncoder_default = JSONEncoder.default
+
 
 def JSONEncoder_new(self, o):
     if isinstance(o, UUID): return str(o)
     if isinstance(o, Decimal): return format(o, '.8f')
     return JSONEncoder_default(self, o)
 
+
 JSONEncoder.default = JSONEncoder_new
 
-# Define o nome do modulo
+# Define the module name
 PACKAGE_NAME = 'exchange_core'
 
 # Monkey patch default_app_config django_otp
 django_otp.plugins.otp_totp.default_app_config = PACKAGE_NAME + '.apps.OTPConfig'
 
-# Diz ao Django aonde está a configuração desse modulo
+# Tells to django where is the module config class
 default_app_config = PACKAGE_NAME + '.apps.Config'
 
-# Armazena o nome do projeto
+# Stores the project name
 settings.PROJECT_NAME = config('PROJECT_NAME')
 
-# Admins do sistema, recebem e-mails de erros 500
+# System admins receive the email errors
 settings.ADMINS = [(v, v) for v in config('ADMINS', default=[], cast=config.list)]
 
-# Configura a view padrao para ser exibida como home para o usuario
+# Configure the default home view for be visualized by the users
 settings.HOME_VIEW = config('HOME_VIEW', default='core>wallets')
 
-# Configura o codigo de acompanhamento do Google Analytics
+# Configure the Google Analytics tracking id
 settings.GOOGLE_ANALYTICS_TRACK_ID = config('GOOGLE_ANALYTICS_TRACK_ID', default=None)
 
-# Configura a obrigatoriedade no envio dos documentos
+# Configure the obligation for require the user documents from users new account
 settings.REQUIRE_USER_DOCUMENTS = config('REQUIRE_USER_DOCUMENTS', default=True, cast=config.boolean)
 
-# Ignore URLs
+# URLs who should be ignored by the exchange middlewares
 settings.IGNORE_PATHS = config('IGNORE_PATHS', default=[], cast=config.list)
 
-# Dominio do site
+# Site domain
 settings.DOMAIN = config('DOMAIN', default='example.com')
 
-# Dominio do site
+# Enable/disable signup
 settings.ENABLE_SIGNUP = config('ENABLE_SIGNUP', default=True, cast=config.boolean)
 
 # https://github.com/Bouke/django-two-factor-auth
@@ -69,11 +71,11 @@ settings.ENABLE_SIGNUP = config('ENABLE_SIGNUP', default=True, cast=config.boole
 # https://github.com/dstufft/django-passwords
 # https://github.com/anymail/django-anymail
 # https://github.com/yourlabs/django-session-security/
-# django.contrib.sites é requerido pelo django-user-accounts
+# django.contrib.sites is required by django-user-accounts
 settings.INSTALLED_APPS += [
     'django.contrib.humanize',
     'django.contrib.sites',
-	'django_otp',
+    'django_otp',
     'django_otp.plugins.otp_static',
     'django_otp.plugins.otp_totp',
     'dj_pagination',
@@ -89,7 +91,7 @@ settings.INSTALLED_APPS += [
     'bootstrap_pagination',
 ]
 
-# Adiciona os middlewares do Two Factor para habilitar a autenticação em dois passos
+# Add the Two Factor middlewares for enable two steps authentication
 settings.MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -101,14 +103,14 @@ settings.MIDDLEWARE += [
     'dj_pagination.middleware.PaginationMiddleware',
 ]
 
-# Define a model do usuário como sendo a model Users desse modulo
+# Defines the user default Model
 settings.AUTH_USER_MODEL = PACKAGE_NAME + '.Users'
 
-# Define o template do Two Factor para ser usado no login
+# Defines the two factor template to be used for login
 settings.LOGIN_URL = 'two_factor:login'
 settings.LOGIN_REDIRECT_URL = reverse_lazy(config('LOGIN_REDIRECT_URL', default='core>wallets'))
 
-# Configuracoes de banco de dados
+# Database config
 settings.DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 settings.DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
@@ -118,10 +120,9 @@ settings.SESSION_REDIS = config('REDIS_URL', default=None, cast=redis_url)
 if settings.SESSION_REDIS:
     settings.SESSION_ENGINE = 'redis_sessions.session'
 
-# Adiciona o contexto do pacote django-user-accounts para os templates
-# Adiciona o contexto do pacote django-session-security para os templates
-# Adiciona o contexto para o nome do projeto
-# Adiciona o contexto para as configuracoes do projeto
+# Adds django-user-accounts to the templates context
+# Adds django-session-security to the templates context
+# Adds the context for project configurations
 settings.TEMPLATES[0]['OPTIONS']['context_processors'] += [
     'account.context_processors.account',
     'exchange_core.context_processors.exchange',
@@ -132,10 +133,10 @@ settings.TEMPLATES[0]['DIRS'] = [
     os.path.join(os.path.dirname(__file__), 'templates'),
 ]
 
-# Define o ID do site
+# Defines default site id
 settings.SITE_ID = 1
 
-# Django user accounts configurações
+# Django-user-accounts config
 settings.ACCOUNT_LOGIN_URL = 'two_factor:login'
 settings.ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = 'two_factor:login'
 settings.ACCOUNT_PASSWORD_RESET_REDIRECT_URL = 'two_factor:login'
@@ -143,10 +144,11 @@ settings.ACCOUNT_SETTINGS_REDIRECT_URL = 'core>settings'
 settings.ACCOUNT_EMAIL_UNIQUE = True
 settings.ACCOUNT_EMAIL_CONFIRMATION_REQUIRED = True
 settings.ACCOUNT_EMAIL_CONFIRMATION_URL = 'core>email-confirm'
-settings.ACCOUNT_PASSWORD_EXPIRY = config('ACCOUNT_PASSWORD_EXPIRY', cast=int) # As senhas expiram em x dias e precisam ser trocas após esse tempo
+settings.ACCOUNT_PASSWORD_EXPIRY = config('ACCOUNT_PASSWORD_EXPIRY',
+                                          cast=int)  # As senhas expiram em x dias e precisam ser trocas após esse tempo
 settings.ACCOUNT_PASSWORD_USE_HISTORY = False
 
-# Django Anymail configurações
+# Django Anymail config
 settings.ANYMAIL = {
     'MAILGUN_API_KEY': config('MAILGUN_API_KEY'),
     'MAILGUN_SENDER_DOMAIN': config('MAILGUN_SENDER_DOMAIN'),
@@ -157,15 +159,16 @@ settings.SERVER_EMAIL = config('SERVER_EMAIL', default='server@example.com')
 settings.DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 settings.EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 
-# Django session security configurações
+# Django session security config
 settings.SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-settings.SESSION_SECURITY_EXPIRE_AFTER = config('SESSION_SECURITY_EXPIRE_AFTER', cast=int) # Define o tempo de inatividade máximo do usuário, caso ele ultrapasse esse tempo, ele deverá fazer login novamente
+settings.SESSION_SECURITY_EXPIRE_AFTER = config('SESSION_SECURITY_EXPIRE_AFTER',
+                                                cast=int)  # Define o tempo de inatividade máximo do usuário, caso ele ultrapasse esse tempo, ele deverá fazer login novamente
 settings.SESSION_SECURITY_WARN_AFTER = config('SESSION_SECURITY_WARN_AFTER', cast=int)
 
-# Django passwords configurações
+# Django passwords config
 settings.PASSWORD_MIN_LENGTH = 8
 
-# Django messages configurações / Alertas do bootstrap
+# Django messages config/ bootstrap alerts
 settings.MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
     messages.INFO: 'alert-info',
@@ -174,14 +177,14 @@ settings.MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
 }
 
-# Configurações de armazenamento
+# Storage config
 settings.DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 settings.AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 settings.AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 settings.AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-# Thumbnail configurações
+# Thumbnail config
 settings.THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 settings.THUMBNAIL_ALIASES = {
     '': {
@@ -189,10 +192,10 @@ settings.THUMBNAIL_ALIASES = {
     },
 }
 
-# Static files configurações
+# Static files config
 settings.STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Opcoes de seguranca
+# Security options
 settings.SECURE_HSTS_SECONDS = config('SECURE_HSTS_SECONDS', default=True, cast=config.boolean)
 settings.SECURE_CONTENT_TYPE_NOSNIFF = config('SECURE_CONTENT_TYPE_NOSNIFF', default=True, cast=config.boolean)
 settings.SECURE_BROWSER_XSS_FILTER = config('SECURE_BROWSER_XSS_FILTER', default=True, cast=config.boolean)
@@ -205,10 +208,10 @@ settings.CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE', default=True, cast=co
 settings.CSRF_COOKIE_HTTPONLY = config('CSRF_COOKIE_HTTPONLY', default=True, cast=config.boolean)
 settings.X_FRAME_OPTIONS = config('X_FRAME_OPTIONS', default='DENY')
 
-# Configura o prefixo da URL do admin
+# Configure the URL prefix for exchange admin
 settings.ADMIN_URL_PREFIX = config('ADMIN_URL_PREFIX', default='admin/')
 
-# Configuracoes de i18n
+# I18N config
 settings.LANGUAGE_CSS_CLASSES = config('LANGUAGE_CSS_CLASSES', default='', cast=pairs)
 settings.LOCALE_PATHS = (os.path.join(settings.BASE_DIR, 'locale'),)
 settings.LANGUAGE_CODE = 'en'
@@ -218,15 +221,15 @@ settings.LANGUAGES = [
     ('es', _('Spanish')),
 ]
 
-# Configurações de l10n
+# L10N config
 settings.USE_L10N = True
 settings.USE_THOUSAND_SEPARATOR = True
 
-# Configuracoes da lib GDAL
+# GDAL library config
 settings.GDAL_LIBRARY_PATH = config('GDAL_LIBRARY_PATH', default=None)
 settings.GEOS_LIBRARY_PATH = config('GEOS_LIBRARY_PATH', default=None)
 
-# Configuracoes do Django Cities
+# Django cities config
 # https://github.com/coderholic/django-cities
 settings.CITIES_LOCALES = ['LANGUAGES']
 settings.CITIES_POSTAL_CODES = []
