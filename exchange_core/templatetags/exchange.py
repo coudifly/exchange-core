437935a6054dbf696d8b1131b70d802f69baf280
Juliano Gouveia <juliano@neosacode.com>
2018-06-21 16:48:35 -0300

    Add decimal_slice filter

--- exchange_core/templatetags/exchange.py
+++ exchange_core/templatetags/exchange.py
@@ -1,22 +1,27 @@
 import json
-import importlib
 
 from django.template import Library
-from django.conf import settings
-from django.urls.resolvers import URLPattern
 
 from exchange_core.utils import get_url_patterns
 
-
 register = Library()
+
 
 @register.filter(is_safe=True)
 def serialize(data):
     return json.dumps(data)
 
 
+@register.filter(is_safe=True)
+def decimal_slice(value, places=2):
+    parts = '{:8f}'.format(value).split('.')
+    return '{}.{}'.format(parts[0], parts[1][:places])
+
+
 # Armazena o nome das URLs para usar como referencia
 pattern_names = get_url_patterns()
+
+
 # Retorna um booleano identificando se a URL existe ou nao
 @register.simple_tag
 def ex_url_exists(pattern_name):
